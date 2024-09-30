5798a7e5138309b2fcf333a8c7bada284f27de5d
Oladotun Olajimbiti <oladotunhero@gmail.com>
2024-09-29 22:24:46 -0700

    created a simple grocery calculator based on recored values

---
+++ Python_Projects/simple_grocery.py
@@ -0,0 +1,32 @@
+# Write your code here
+prices= """Prices:
+Bubblegum: $2
+Toffee: $0.2
+Ice cream: $5
+Milk chocolate: $4
+Doughnut: $2.5
+Pancake: $3.2
+"""
+print(prices)
+
+earned= """
+Earned amount:
+Bubblegum: $202
+Toffee: $118
+Ice cream: $2250
+Milk chocolate: $1680
+Doughnut: $1075
+Pancake: $80
+"""
+print(earned)
+
+#remove value from the string dictionary and sum it out
+lines = earned.strip().split('\n')
+income = []
+for line in lines:
+    if "$" in line:
+        amount = line.split('$')[-1].strip()
+        income.append(float(amount))
+total_earning = sum(income)
+
+print(f"Income: {total_earning}")
