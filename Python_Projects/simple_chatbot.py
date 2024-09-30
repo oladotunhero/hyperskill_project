ea8beadc1db7a66985a4ce5bab6989cc1696dd5b
Oladotun Olajimbiti <oladotunhero@gmail.com>
2024-09-29 23:05:14 -0700

    started the simple python chatbot project

---
+++ Python_Projects/simple_chatbot.py
@@ -0,0 +1,5 @@
+# write your code here
+bot_name = input("enter the name of your bot:")
+birth_year = int(input("Enter the year you created the bot"))
+
+print(f"Hello! My name is {bot_name}.\nI was created in {birth_year}.")
