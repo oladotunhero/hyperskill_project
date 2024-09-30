5a6db6debcacbadbb0a60bfe015ace8398469f32
Oladotun Olajimbiti <oladotunhero@gmail.com>
2024-09-30 01:34:40 -0700

    Created two fuctions to handle the bot creation and user name

--- Python_Projects/simple_chatbot.py
+++ Python_Projects/simple_chatbot.py
@@ -1,5 +1,16 @@
-# write your code here
-bot_name = input("enter the name of your bot:")
-birth_year = int(input("Enter the year you created the bot"))
+def greet(bot_name, created_year ):
+    print(f"Hello! My name is {bot_name}.\nI was created in {created_year}.")
 
-print(f"Hello! My name is {bot_name}.\nI was created in {birth_year}.")
+
+def remind_name():
+    print("Please, remind me your name.")
+    # reading a name
+    your_name = input()
+    print(f"What a great name you have, {your_name}!")
+
+
+# Now we can use these functions
+chat_bot_name = input("enter the name of your bot: \n")
+chat_bot_year = int(input("Enter the year you created the bot: \n"))
+greet(chat_bot_name, chat_bot_year)
+remind_name()
